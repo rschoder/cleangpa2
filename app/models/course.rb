52bbd0e604b273cccd4015d430a25bc5c54588b6
rschoder <rob.schoder@gmail.com>
2015-12-07 23:24:05 -0600

    With course database

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ActiveRecord::Base
 
   has_many :grades , :class_name => "Grade", :foreign_key => "course_id"
 
+  validates :course_name, :presence => true, :uniqueness => true
+
+  validates_uniqueness_of :course_name, :case_sensitive => false
+
 end
