63ba0968a9fbe418ae75bfa1a86c52da20343133
rschoder <rob.schoder@gmail.com>
2015-12-10 00:18:41 -0600

    w removed code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @gradelet = @user.grades
-    @coursename = @grades.course_name
+
    end
 
   def signout
